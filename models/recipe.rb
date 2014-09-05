2a86625634149c6fbba83fc6cde7a07a9d4e8759
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-05 11:53:59 -0400

    added first method

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,20 @@
+require 'pg'
+
 class Recipe
+  def self.all
+
+
+  end
+
+  def db_connection
+    begin
+    connection = PG.connect(dbname: 'recipes')
+
+    yield(connection)
+
+   ensure
+    connection.close
+   end
+  end
+
 end
